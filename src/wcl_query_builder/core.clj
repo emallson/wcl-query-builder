75dad64964e92455dfa4688475551ccbd1d985e7
emallson <emallson@atlanis.net>
2020-06-14 16:42:40 -0400

    basically done with the initial implementation.
    need a couple of functions to actually exec these, but mapping those
    from the TS implementations should be easy enough.

--- src/wcl_query_builder/core.clj
+++ src/wcl_query_builder/core.clj
@@ -20,12 +20,15 @@
 ;;     (group-by :evoke-cast))
 ;;
 
+(def nil-query {::filter nil
+                ::post identity})
+
 (defn select
   "Select a subset of the data. Multiple selections are implicitly ANDed together."
   [{flt ::filter
     post ::post}
-   {fields ::fields
-    where ::where}]
+   {fields :fields
+    where :where}]
   {::filter (q/and flt where)
    ::post (if fields
             (comp (fn [events]
@@ -38,29 +41,167 @@
     to :to
     by :group-by
     on :group-on
+    key :key
     :as range}
+   idx
    events]
-  (let [[_ [start & rst]] (split-with #(q/eval-not % from) events)]
-    (when start
-      (let [[group-term entity] (q/group-filter start by)
+  (let [[head [start & rst]] (split-with #(q/eval-not % from) events)]
+    (if start
+      (let [rst (post-bracketed-range range (inc idx) rst)
+            [group-term entity] (q/group-filter start by)
             on-term (if (or (nil? on) (nil? entity))
                       group-term
                       (q/eq (q/group-key on) entity))
-            [body [end & _]] (split-with #(q/eval-not % (q/and to group-term)) rst)]
-        (conj (post-bracketed-range range rst)
-              {:start start
-               :body (filter #(q/eval % on-term) body)
-               :end end})))))
+            [body [end & tail]] (split-with #(q/eval-not % (q/and to group-term)) rst)]
+        (concat head
+                [(update-in start [:range key :start] conj idx)]
+                (map (fn [event]
+                       (if (q/eval event on-term)
+                         (update-in event [:range key :match] conj idx)
+                         event))
+                     body)
+                [(update-in end [:range key :end] conj idx)]
+                tail))
+      events)))
 
-(defn within-bracketed-range
+(defn filter-to-range
+  "Filter out all events that do not belong to a specific range."
+  [key events]
+  (filter #(some? (get-in % [:range key])) events))
+
+(defmulti within-range
   "Selects data that lies within a given range."
+  (fn [_ {type :range/type}] type))
+
+(defmethod within-range :bracketed
   [{flt ::filter
     post ::post}
-   {from ::from
-    to ::to
+   {from :from
+    to :to
+    key :key
     :as range}]
   (if (q/contains-range flt)
     {::filter (q/or flt from to)
-     ::post (comp post-bracketed-range post)}
+     ::post (comp
+             (partial filter-to-range key)
+             (partial post-bracketed-range range 0)
+             post)}
     {::filter (q/and flt (q/range range))
-     ::post post}))
+     ::post (comp
+             (partial post-bracketed-range range 0) ; add range indices
+             post)}))
+
+(defn dissoc-all
+  [map keys]
+  (apply dissoc map keys))
+
+(defn post-delimited-range
+  "Post-processing for a delimited range."
+  [{delim :delimiter
+    by :group-by
+    on :group-on
+    key :key
+    :as range}
+   events]
+                                        ; in theory this should be a for, but i need to track the group idx
+  (loop [event (first events)
+         rst (rest events)
+         prev []
+         idx 0
+         matchers {}
+         ends {}]
+    (let [flt (fn [[idx term]]
+                (q/eval event term))
+          matching (->> matchers (filter flt) (map first))
+          ending (->> ends (filter flt) (map first))
+          [end-term entity] (when (q/eval event delim)
+                              (q/group-filter event by))
+          on-term (if (or (nil? on) (nil? entity))
+                    end-term
+                    (q/eq (q/group-key on) entity))
+          result-event (-> event
+                           (update-in [:range key :match]
+                                      #(apply conj % matching))
+                           (update-in [:range key :end]
+                                      #(apply conj % ending))
+                           (update-in [:range key :start]
+                                      #(if (some? end-term)
+                                         (conj % idx)
+                                         %)))]
+      (if (empty? rst)
+        (conj prev result-event)
+        (recur
+         (first rst)
+         (rest rst)
+         (conj prev result-event)
+         (if (some? end-term) (inc idx) idx)
+         (cond-> matchers
+           true (dissoc-all matching)
+           (some? on-term) (assoc idx on-term))
+         (cond-> ends
+           true (dissoc-all ending)
+           (some? end-term) (assoc idx (q/and delim end-term))))))))
+
+(defmethod within-range :delimited
+  [{flt ::filter
+    post ::post}
+   {key :key
+    delim :delimiter
+    :as range}]
+  {::filter (q/or flt delim)
+   ::post (comp
+           (partial filter-to-range key)
+           (partial post-delimited-range range)
+           post)})
+
+(defmulti tag-range
+  "Compute ranges and annotate them with tags. Items outside any range are still included."
+  (fn [_ {type :range/type}] type))
+
+(defmethod tag-range :bracketed
+  [{flt ::filter
+    post ::post}
+   {from :from
+    to :to
+    key :key
+    :as range}]
+  {::filter (q/or flt from to)
+    ::post (comp
+            (partial post-bracketed-range range 0)
+            post)})
+
+(defmethod tag-range :delimited
+  [{flt ::filter
+    post ::post}
+   {key :key
+    delim :delimiter
+    :as range}]
+  {::filter (q/or flt delim)
+   ::post (comp
+           (partial filter-to-range key)
+           (partial post-delimited-range range)
+           post)})
+  
+(defn run-query
+  "Runs a textual query against WCL."
+  [text api-key start-ts stop-ts]
+  [])
+
+(defn split-fights
+  "Split an event sequence into multiple sequences based on fight start timestamps."
+  [events fight-breaks]
+  events)
+
+(defn exec
+  "Execute a query."
+  [{flt ::filter
+    post ::post}
+   api-key
+   start
+   stop
+   fight-breaks]
+  (let [txt (q/->text flt)]
+    (as-> txt t
+        (run-query t api-key start stop)
+        (split-fights t fight-breaks)
+        (map post t))))
--- test/wcl_query_builder/core_test.clj
+++ test/wcl_query_builder/core_test.clj
@@ -4,53 +4,87 @@
             [wcl-query-builder.core :as wcl]))
 
 (def dumb-events
-  (map #(assoc %1 :timestamp %2)
-      [{:target {:id 1}
-        :source {:id 2}
-        :type :applybuff
-        :ability {:guid 123
-                  :name "Test"}}
-       {:target {:id 2}
-         :source {:id 1}
-         :type :damage
-         :ability {:guid 1
-                   :name "Melee"}}
-       {:target {:id 3}
-        :source {:id 2}
-        :type :applybuff
-        :ability {:guid 123
-                  :name "Test"}}
-       {:target {:id 1}
-        :source {:id 2}
-        :ability {:guid 123
-                  :name "Test"}
-        :type :removebuff}
-       {:target {:id 2}
-        :source {:id 3}
-        :type :heal
-        :ability {:guid 1234}}
-       {:target {:id 47}
-        :source {:id 48}
-        :type :junk}
-       {:target {:id 3}
-        :source {:id 2}
-        :type :removebuff
-        :ability {:guid 123
-                  :name "Test"}}]
-      (range)))
+  [{:target {:id 1}
+    :source {:id 2}
+    :type :applybuff
+    :ability {:guid 123
+              :name "Test"}}
+   {:target {:id 2}
+     :source {:id 1}
+     :type :damage
+     :ability {:guid 1
+               :name "Melee"}}
+   {:target {:id 3}
+    :source {:id 2}
+    :type :applybuff
+    :ability {:guid 123
+              :name "Test"}}
+   {:target {:id 1}
+    :source {:id 2}
+    :ability {:guid 123
+              :name "Test"}
+    :type :removebuff}
+   {:target {:id 2}
+    :source {:id 3}
+    :type :heal
+    :ability {:guid 1234}}
+   {:target {:id 47}
+    :source {:id 48}
+    :type :junk}
+   {:target {:id 3}
+    :source {:id 2}
+    :type :removebuff
+    :ability {:guid 123
+              :name "Test"}}])
+      
 
 (def dumb-solution
-  (list {:start (first dumb-events)
-                :body (list (second dumb-events))
-                :end (nth dumb-events 3)}
-        {:start (nth dumb-events 2)
-          :body (list (nth dumb-events 4))
-          :end (last dumb-events)}))
+  [{:target {:id 1}
+    :source {:id 2}
+    :type :applybuff
+    :range {:buff '(0)}
+    :ability {:guid 123
+              :name "Test"}}
+   {:target {:id 2}
+     :source {:id 1}
+     :type :damage
+     :range {:buff '(0)}
+     :ability {:guid 1
+               :name "Melee"}}
+   {:target {:id 3}
+    :source {:id 2}
+    :type :applybuff
+    :range {:buff '(1)}
+    :ability {:guid 123
+              :name "Test"}}
+   {:target {:id 1}
+    :source {:id 2}
+    :ability {:guid 123
+              :name "Test"}
+    :range {:buff '(0)}
+    :type :removebuff}
+   {:target {:id 2}
+    :source {:id 3}
+    :type :heal
+    :range {:buff '(1)}
+    :ability {:guid 1234}}
+   {:target {:id 47}
+    :source {:id 48}
+    :type :junk}
+   {:target {:id 3}
+    :source {:id 2}
+    :type :removebuff
+    :range {:buff '(1)}
+    :ability {:guid 123
+              :name "Test"}}])
+  
 
 (deftest test-basic-bracketed-range
   (is (= dumb-solution
          (wcl/post-bracketed-range {:from (q/eq :type :applybuff)
                                     :to (q/eq :type :removebuff)
                                     :group-by :target
-                                    :group-on :source}
+                                    :group-on :source
+                                    :key :buff}
+                                   0
                                    dumb-events))))
